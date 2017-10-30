dde38efad5071554b2fe2eb7bf12cd54eebf02fd
gauwart <gauwart@gmail.com>
2017-10-30 22:47:16 +0100

    Update JavaApplication40.java

--- JavaApplication40/src/javaapplication40/JavaApplication40.java
+++ JavaApplication40/src/javaapplication40/JavaApplication40.java
@@ -7,8 +7,10 @@ public class JavaApplication40 {
    
     public static void main(String[] args) {
         String hola = "Hola mundo";
+       
         
         System.out.println(hola);
+        JOptionPane.showMessageDialog(null,"Hola mundo");
         
     }
     
